443a2e4c12bb8b18e1dac600e90c7cf5afb64c5b
Idevaldo <inetomec@yahoo.com.br>
2022-05-01 17:48:49 -0300

    feat: :sparkles: Overriding Express' Request Object

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 import authConfig from "@config/auth";
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 const isAuthenticated = (
   request: Request,
   response: Response,
@@ -18,6 +24,12 @@ const isAuthenticated = (
 
   try {
     const decodedToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedToken as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
 
     return next();
   } catch {
