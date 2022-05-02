8f1042c6692f39d71985055686f66b9305d7dca4
Idevaldo <inetomec@yahoo.com.br>
2022-05-02 16:46:24 -0300

    feat: :sparkles: Sending Email for Password Recovery

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../typeorm/repositories/UsersRepository";
 import { UserTokensRepository } from "../typeorm/repositories/UserTokensRepository";
 import { EtherealMail } from "@config/mail/EtherealMail";
+import path from "path";
 
 interface IRequest {
   email: string;
@@ -21,7 +22,12 @@ class SendForgotPasswordEmailService {
 
     const { token } = await userTokensRepository.generate(user.id);
 
-    // console.log(token);
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      "..",
+      "views",
+      "forgot_password.hbs",
+    );
 
     await EtherealMail.sendMail({
       to: {
@@ -30,10 +36,10 @@ class SendForgotPasswordEmailService {
       },
       subject: "[API Vendas] Recuperação de Senha",
       templateData: {
-        template: `Olá {{name}}: {{token}}`,
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
